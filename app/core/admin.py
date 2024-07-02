5a6f629788e7adcfb9033ce89d9f634f6cb14645
miloram <miloram04@gmail.com>
2024-07-02 15:51:00 -0700

    lesson 57

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,7 +3,7 @@ Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import getttext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -27,5 +27,19 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldssets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 admin.site.register(models.User, UserAdmin)
